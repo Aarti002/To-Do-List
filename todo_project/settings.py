2c1e26dcabf50d3f2ada787d5c733663c7ed08da
Aarti <aartikumarisingh120@gmail.com>
2022-04-20 09:53:15 +0530

    second commit

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -76,13 +76,15 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
+
+"""
 
 
 DATABASES = {
@@ -96,7 +98,7 @@ DATABASES = {
     }
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
